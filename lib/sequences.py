3662af2939dca670eeeced79988db783c6a7cde7
Erick Koome <koomembwiria@gmail.com>
2024-06-04 04:49:44 +0300

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    """Prints a list of the first n Fibonacci numbers."""
+    if length <= 0:
+        print([])
+        return
+
+    # The first two Fibonacci numbers
+    fibonacci_list = [0, 1]
+
+    # Generate the rest of the Fibonacci sequence up to length
+    while len(fibonacci_list) < length:
+        next_value = fibonacci_list[-1] + fibonacci_list[-2]
+        fibonacci_list.append(next_value)
+
+    # Print the Fibonacci sequence
+    print(fibonacci_list[:length])
